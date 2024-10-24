93046f15db9f628043a75d5bdf30ffa777e7619c
felpsrlq <zhigortroll777@gmail.com>
2024-10-23 21:20:51 -0300

    primeira aula de PHP

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>introdução ao PHP</title>
+</head>
+<body>
+    
+<h1>Aula de introdução ao PHP</h1>
+<?php 
+$nome = "sabrine";
+$sobrenome = "picanha";
+$idade = 19;
+$genero = "vinagrete";
+
+echo $nome." ".$sobrenome;
+echo "<br>";
+echo $idade ;
+echo "<br>";
+echo $genero;
+echo "<br>";
+$num1 =50;
+$num2 = 50;
+echo "-----------------------------";
+echo "<br>";
+echo "resultado: ".$num1+$num2;
+
+?>
+
+</body>
+</html>
